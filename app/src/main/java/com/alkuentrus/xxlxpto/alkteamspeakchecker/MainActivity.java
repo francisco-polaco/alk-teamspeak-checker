4fec3484319ed68a073715a86bd89a3d40ba6de9
Francisco Santos <franciscopolaco@ist.utl.pt>
2015-12-15 21:53:42 +0000

    Initial commit

---
+++ app/src/main/java/com/alkuentrus/xxlxpto/alkteamspeakchecker/MainActivity.java
@@ -0,0 +1,77 @@
+package com.alkuentrus.xxlxpto.alkteamspeakchecker;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String WEB_TS_CHECKER = "http://web.ist.utl.pt/~ist179719/ts/mobile.html";
+    private WebView mWebView;
+    private FloatingActionButton mRefreshButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        mWebView = (WebView) findViewById(R.id.webView);
+        mRefreshButton = (FloatingActionButton) findViewById(R.id.refresh);
+
+        setEventOnClickListeners();
+
+    }
+
+    private void setEventOnClickListeners(){
+        mRefreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebView.reload();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        if (Build.VERSION.SDK_INT >= 19) {
+            webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        }
+        mWebView.loadUrl(WEB_TS_CHECKER);
+        mRefreshButton.setClickable(true);
+
+    }
+}
